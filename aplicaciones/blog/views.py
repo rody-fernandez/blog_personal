14f65967e52be4129c7967f2d9a082f93ba08dff
Rody_Fernandez <rody.fernandez.saa@gmail.com>
2020-03-04 15:00:34 -0300

    Corecciones en algunas lineas (nombre__iexact e get_object_or_404

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,44 +1,42 @@
 from django.shortcuts import render
 from .models import Post, Categoria
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
     posts = Post.objects.filter(estado = True)
     return render(request, 'index.html',{'posts':posts})
 
+def detallePost(request, slug):
+    post = get_object_or_404(Post, slug = slug)
+    return render(request, 'post.html',{'detalle_post':post})
+
 def generales(request):
     posts = Post.objects.filter(
         estado = True,
-        categoria = Categoria.objects.get(nombre = 'Generales'))
+        categoria = Categoria.objects.get(nombre__iexact = 'Generales'))
     return render(request, 'generales.html', {'posts':posts})
 
 def tutoriales(request):
     posts = Post.objects.filter(
         estado = True,
-        categoria = Categoria.objects.get(nombre = 'Tutoriales'))
+        categoria = Categoria.objects.get(nombre__iexact = 'Tutoriales'))
     return render(request, 'tutoriales.html',{'posts':posts})
 
 def tecnologia(request):
     posts = Post.objects.filter(
         estado = True,
-        categoria = Categoria.objects.get(nombre = 'Tecnologia'))
+        categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia'))
     return render(request, 'tecnologia.html',{'posts':posts})
 
 def programacion(request):
     posts = Post.objects.filter(
         estado = True,
-        categoria = Categoria.objects.get(nombre = 'Programacion'))
+        categoria = Categoria.objects.get(nombre__iexact = 'Programacion'))
     return render(request, 'programacion.html',{'posts':posts})    
 
 def videojuegos(request):
     posts = Post.objects.filter(
         estado = True,
-        categoria = Categoria.objects.get(nombre = 'Video Juegos'))
+        categoria = Categoria.objects.get(nombre__iexact = 'Video Juegos'))
     return render(request, 'videojuegos.html',{'posts':posts})    
-
-def detallePost(request, slug):
-    post = Post.objects.get(
-        slug = slug
-    )
-    print(post)
-    return render(request, 'post.html',{'detalle_post':post})
