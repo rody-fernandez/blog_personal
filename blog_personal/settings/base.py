794a5966f232b0404cdb3cb6703c6f780cbb9c29
Rody <rody.fernandez.saa@gmail.com>
2020-03-29 15:53:44 -0400

    Creacion carpeta settings con archivos local,base,production para Deploy a Heroku

--- blog_personal/settings/base.py
+++ blog_personal/settings/base.py
@@ -71,19 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'blog_personal.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -128,4 +115,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
